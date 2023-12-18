016c2efee753782f95801a7866d0553e9be8cde5
Mr-Fougere <alexandre7822@gmail.com>
2023-12-18 20:18:46 +0100

    refacto feature integration

--- scripts/videoDetector.js
+++ scripts/videoDetector.js
@@ -1,32 +1,39 @@
-const videoPlayer = {
-  _value: null,
+const adsSkipper = new AdsSkipper();
+const resolutionReducer = new ResolutionReducer();
+const actionIntegrer = new ActionIntegrer();
+let mainPlayer;
+const ADS_CLASSES = "ytp-ads-button ytp-button";
 
-  get value() {
-    return this._value;
-  },
-
-  set value(newValue) {
-    if (newValue !== this._value) {
-      this._value = newValue;
-      this.onUpdate(newValue);
-    }
-  },
-
-  onUpdate(newValue) {
-    integrateButtons(newValue);
-    updaterBlurListeners();
-  },
-};
-
-const handleNewVideo = (videoElement) => {
-  if (!videoElement) return;
-  if (videoPlayer.value) return;
-  videoPlayer.value = videoElement;
-};
-
+let built = false;
 const config = {
   childList: true,
   subtree: true,
+};
+
+const setMainPlayer = async() => {
+  let mainPlayerTries = 0;
+  return new Promise((resolve, reject) => {
+    setInterval(() => {
+      mainPlayerTries++;
+      mainPlayer = document.querySelector("DIV#player");
+      if (mainPlayer) {
+        resolve(mainPlayer);
+      }
+      if (mainPlayerTries > 50) reject();
+    }, 100);
+  });
+};
+
+const buildService = (node) => {
+  if (built) return;
+  if (!node) return;
+  if (node.parentNode.parentNode.id != "movie_player") return;
+
+  actionIntegrer.integrateButtons(adsSkipper, resolutionReducer, node);
+  adsSkipper.setPlayers(mainPlayer, node);
+  resolutionReducer.setListeners(mainPlayer, node);
+  node.addEventListener("playing", adsSkipper.skipAd);
+  built = true;
 };
 
 const mutationCallback = (mutationsList, _observer) => {
@@ -34,15 +41,24 @@ const mutationCallback = (mutationsList, _observer) => {
     if (mutation.type === "childList") {
       mutation.addedNodes.forEach((node) => {
         if (node instanceof HTMLVideoElement) {
-          handleNewVideo(node);
+          buildService(node);
+        }
+        if (node instanceof HTMLElement) {
+          if (node.classList.value == ADS_CLASSES) {
+            adsSkipper.skipAd();
+          }
         }
       });
     }
   }
 };
 
-const obs = new MutationObserver(mutationCallback);
+const setup = async () => {
+  const obs = new MutationObserver(mutationCallback);
+  mainPlayer = await setMainPlayer();
+  console.log(mainPlayer);
+  obs.observe(mainPlayer, config);
+  buildService(mainPlayer.querySelector("VIDEO.video-stream.html5-main-video"));
+};
 
-obs.observe(document.body, config);
-
-handleNewVideo(document.querySelector("VIDEO.video-stream.html5-main-video"));
+setup();
